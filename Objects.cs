977ac524da049ef1608c8a9b2b3dde73c9a46286
Sharon Grossman <shgrossman3@gmail.com>
2022-04-06 10:20:54 +0300

    Added Item class and Item constructor

--- Objects.cs
+++ Objects.cs
@@ -17,4 +17,42 @@ namespace NUnitLessonsSharonSefi
             this.password = password;
         }
     }
+
+    //Item class
+    public class Item
+    {
+        // Item name
+        public string itemName;
+        // Item decsription
+        public string itemDesc;
+        // Item price
+        public double itemPrice;
+        // Item image URL
+        public string itemImage;
+
+        // Cart button - boolean, if true "Add to cart" should be clicked. if false, "Remove" button is there and should not be clicked.
+        // Maybe future idea, not part of item constructor currently!
+        public bool hasAddToCart;
+
+
+        // Item constructor
+        public Item(string itemName, string itemDesc, double itemPrice, string itemImage)
+        {
+            this.itemName = itemName;
+            this.itemDesc = itemDesc;
+            this.itemPrice = itemPrice;
+            this.itemImage = itemImage;
+        }
+
+        // Add to cart logic idea, not relevant right now
+        public void clickAddToCart()
+        {
+            if (hasAddToCart)
+            {
+                //click button
+            }
+            else { }
+            //no click
+        }
+    }
 }
