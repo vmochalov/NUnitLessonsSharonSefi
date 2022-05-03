6bb1958ecd3641725ee70857170d1e47e360e8eb
Sefi Mochalov <mochalov.vsevolod@gmail.com>
2022-05-03 17:14:51 +0300

    TestCaseSource - fixed

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -99,11 +99,24 @@ namespace NUnitLessonsSharonSefi
             loginButton.Click();
         }
 
-
-        [TestCase("Sauce Labs Backpack")]
-        [TestCase("Sauce Labs Bolt T-Shirt")]
-        [TestCase("BullShit that not exist")]
-        public void AddItemToCartByName(string item)
+        public static IEnumerable<TestCaseData> ItemsWithDescriptionsAndPrices
+        {
+            get
+            {
+                yield return new TestCaseData("Sauce Labs Backpack", "carry.allTheThings() with the sleek, " +
+                    "streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.", 29.99);
+                yield return new TestCaseData("Sauce Labs Bolt T-Shirt", "Get your testing superhero on with the Sauce " +
+                    "Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.", 15.99);
+                yield return new TestCaseData("Sauce Labs Onesie", "Rib snap infant onesie for the junior automation " +
+                    "engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.", 7.99);
+                yield return new TestCaseData("Sauce Labs Bike Light", "A red light isn't the desired state in testing but it sure " +
+                    "helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.", 9.99);
+                yield return new TestCaseData("Sauce Labs Fleece Jacket", "It's not every day that you come across a " +
+                    "midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.", 49.99);
+            }
+        }
+        [TestCaseSource(nameof(ItemsWithDescriptionsAndPrices))]
+        public void AddItemToCartByName(string item, string itemDescription, double itemPrice)
         {
             LoginProcess("standard_user", "secret_sauce");
             var itemName = driver.FindElement(By.XPath("//div[normalize-space()='" + item + "']"));
